26ad40ca7dc49d94e201053a935dad307ec6a64a
sengawasenga <14marcel.senga@gmail.com>
2023-08-09 15:04:44 +0100

    first commit

---
+++ script/index.js
@@ -0,0 +1,7 @@
+// hey welcome to this awesome web view of the Creeto company, my name is Marcel and I'm proud that this site pleased you 
+
+
+document.querySelector('button.burger').addEventListener('click', ()=> {
+    document.querySelector('.navbar .links').classList.toggle('active')
+})
+
